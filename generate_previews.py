f4e5e8ac05be18bbc3d4c131d695ab78c27c3977
Till Beemelmanns <till.beemelmanns@rwth-aachen.de>
2020-06-02 16:20:35 +0200

    add script to generate preview

--- generate_previews.py
+++ generate_previews.py
@@ -1,21 +1,26 @@
 import tensorflow as tf
 from input_preprocess import *
-import matplotlib.pyplot as plt
 import cv2
 
 if __name__ == '__main__':
+    img_org = cv2.cvtColor(cv2.imread("assets/test_image.jpg"), cv2.COLOR_BGR2RGB)
 
-    augmentation_policy = {'sub_policy0': {'op0': ['add_noise', 1, 10]}}
+    for op in ALL_AUGMENTATION_NAMES_AS_LIST:
+        augmentation_policy = {}
+        subpolicy = {}
 
-    img = cv2.cvtColor(cv2.imread("assets/test_image.jpg"), cv2.COLOR_BGR2RGB)
+        for level in range(11):
+            subpolicy['op0'] = [op, 1.0, level]
+            augmentation_policy['sub_policy0'] = subpolicy
+            img = tf.convert_to_tensor(img_org)
+            img = tf.cast(img, dtype=tf.uint8)
+            img = apply_augmentation_policy(img, augmentation_policy)
+            img = img.numpy().astype(dtype='uint8')
+            img = cv2.resize(img, (img.shape[1]//4, img.shape[0]//4))
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
-    img = tf.convert_to_tensor(img)
+            cv2.imwrite(f"output/{op}_{level}.jpg", img)
 
-    img = tf.cast(img, dtype=tf.uint8)
 
-    img = apply_augmentation_policy(img, augmentation_policy)
 
-    img = img.numpy()
 
-    plt.imshow(img/255.0)
-    plt.show()

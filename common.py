c26bd782bc57f4b2ef191c80a151cbbc5f9fb551
Till Beemelmanns <till.beemelmanns@rwth-aachen.de>
2021-02-13 21:56:10 +0100

    Restructure the code

--- common.py
+++ common.py
@@ -7,4 +7,5 @@ IMAGE_NAME = 'image_name'
 LABEL = 'label'
 ORIGINAL_IMAGE = 'original_image'
 TEST_IMAGE_PATH = "assets/test_image.jpg"
+SAVE_DIR = "assets"
 PIXEL_VALUE_PAD = [127, 127, 127]
--- utils/generate_previews.py
+++ utils/generate_previews.py
@@ -7,7 +7,7 @@ from augmentation_policies import augmentation_policy
 import common
 
 
-def generate_all_augmentation_gifs(image):
+def generate_all_augmentation_gifs(image, save_dir):
     for op in ALL_AUGMENTATION_NAMES_AS_LIST:
         augmentation_policy = {}
         subpolicy = {}
@@ -21,13 +21,13 @@ def generate_all_augmentation_gifs(image):
             img = img.numpy().astype(dtype='uint8')
             img = cv2.resize(img, (img.shape[1]//4, img.shape[0]//4))
             img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-            cv2.imwrite(f"assets/{op}_{level}.jpg", img)
+            cv2.imwrite(f"{save_dir}/{op}_{level}.jpg", img)
 
-        os.system(f"convert $(ls -1 assets/{op}_*.jpg | sort -V) assets/{op}.gif")
-        os.system(f"rm assets/{op}_*.jpg")
+        os.system(f"convert $(ls -1 {save_dir}/{op}_*.jpg | sort -V) {save_dir}/{op}.gif")
+        os.system(f"rm {save_dir}/{op}_*.jpg")
 
 
-def generate_augmentation_policy_gif(image, policy):
+def generate_augmentation_policy_gif(image, policy, save_dir):
     for i in range(20):
         img = tf.convert_to_tensor(image)
         img = tf.cast(img, dtype=tf.float32)
@@ -35,15 +35,15 @@ def generate_augmentation_policy_gif(image, policy):
         img = img.numpy().astype(dtype='uint8')
         img = cv2.resize(img, (img.shape[1]//4, img.shape[0]//4))
         img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        cv2.imwrite(f"assets/augmentation_policy_{i}.jpg", img)
+        cv2.imwrite(f"{save_dir}/augmentation_policy_{i}.jpg", img)
 
-    os.system(f"convert $(ls -1 assets/augmentation_policy_*.jpg | sort -V) assets/augmentation_policy.gif")
-    os.system(f"rm assets/augmentation_policy_*.jpg")
+    os.system(f"convert $(ls -1 {save_dir}/augmentation_policy_*.jpg | sort -V) {save_dir}/augmentation_policy.gif")
+    os.system(f"rm {save_dir}/augmentation_policy_*.jpg")
 
 
 if __name__ == '__main__':
     img_org = cv2.cvtColor(cv2.imread(common.TEST_IMAGE_PATH), cv2.COLOR_BGR2RGB)
-    generate_augmentation_policy_gif(img_org, augmentation_policy)
-    generate_all_augmentation_gifs(img_org)
+    generate_augmentation_policy_gif(img_org, augmentation_policy, common.SAVE_DIR)
+    generate_all_augmentation_gifs(img_org, common.SAVE_DIR)
 
 
